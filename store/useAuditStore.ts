0e6aba5ed68e926b5909ea9491c264f9c6548919
Hrishi <rathodhrishi12345@gmail.com>
2026-05-07 20:26:06 +0530

    feat: integrate zustand middleware for persistent state management in useAuditStore

--- store/useAuditStore.ts
+++ store/useAuditStore.ts
@@ -1,16 +1,28 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
 import { AuditInput } from "@/types";
 
 interface AuditState {
   data: AuditInput;
+
   setData: (data: AuditInput) => void;
 }
 
-export const useAuditStore = create<AuditState>((set) => ({
-  data: {
-    tools: [],
-    teamSize: 1,
-    useCase: "coding",
-  },
-  setData: (data) => set({ data }),
-}));
+export const useAuditStore = create<AuditState>()(
+  persist(
+    (set) => ({
+      data: {
+        tools: [],
+        teamSize: 1,
+        useCase: "coding",
+      },
+
+      setData: (data) => set({ data }),
+    }),
+
+    {
+      name: "audit-storage",
+    }
+  )
+);
